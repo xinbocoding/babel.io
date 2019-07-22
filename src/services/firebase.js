2c85d7d4c749da7fa5f032def9e99b93be263e13
xinbo wang <xinbo.wang.dt@gmail.com>
2019-07-21 22:09:11 -0700

    Remove firebase token from source code

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 let firebaseService = app.initializeApp({
-  apiKey: "AIzaSyDC5gwqWjAIYYbgt6VXJnAHie6jVLxh_fI",
-  authDomain: "local-firebase-dev.firebaseapp.com",
-  databaseURL: "https://local-firebase-dev.firebaseio.com",
-  projectId: "local-firebase-dev",
-  storageBucket: "",
-  messagingSenderId: "406257357332",
-  appId: "1:406257357332:web:3ea887869e07a0de"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 export default firebaseService;
