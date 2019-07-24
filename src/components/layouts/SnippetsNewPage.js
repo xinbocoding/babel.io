5d1c369fee63c14d9373f170aeb50216014c2dae
Yaodong Zhao <git@yaodong.org>
2019-07-24 11:48:46 -0700

    allow JSX using .js file extension

--- src/components/layouts/SnippetsNewPage.js
+++ src/components/layouts/SnippetsNewPage.js
@@ -3,15 +3,12 @@ import { Container } from '@material-ui/core';
 import SnippetForm from '../snippets/SnippetForm';
 import NavBar from '../NavBar';
 
-class SnippetsNewPage extends React.Component {
-  render() {
-    return (
-      <Container>
-        <NavBar />
-        <SnippetForm />
-      </Container>
-    );
-  }
-}
+const SnippetsNewPage = () => (
+  <Container>
+    <NavBar />
+    <SnippetForm />
+  </Container>
+);
+
 
 export default SnippetsNewPage;
