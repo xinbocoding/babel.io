973b0875ffa73f5bbf0b2f9f562e614932954718
Yaodong Zhao <git@yaodong.org>
2019-09-01 21:09:54 -0700

    preflight

--- src/components/Elements/LandingDemo.js
+++ src/components/Elements/LandingDemo.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 
 export default class LandingDemo extends Component {
   render() {
-    return <div className="text-muted" />;
+    return (
+      <div className="text-muted">
+        <div className="alert alert-danger" role="alert">
+          This site is in beta. In the meantime, please report bugs
+{' '}
+          <a href="https://github.com/xinbocoding/babel.io/issues">here</a>
+.
+</div>
+      </div>
+    );
   }
 }
